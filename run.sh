21afefa907f41c12975ba6ff384cfd6ed1c50d50
maryam <monyabdullah98@gmail.com>
2025-12-09 21:09:03 +0400

    Updated project structure and added new features

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-export FLASK_APP=app
-export FLASK_ENV=development
-flask run --host=0.0.0.0 --port=5000
