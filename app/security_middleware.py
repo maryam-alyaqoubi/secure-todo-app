36f092c3361a957b96c6a32a3b6360e0f14792d2
‪mony abdullah‬‏ <monyabdullah98@gmail.com>
2025-12-09 21:16:17 +0400

    Add security headers utility function

---
+++ app/security_middleware.py
@@ -0,0 +1,9 @@
+# small utilities for headers and security helpers
+from flask import request, make_response
+
+def add_security_headers(response):
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Referrer-Policy'] = 'no-referrer'
+    response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self'"
+    return response
